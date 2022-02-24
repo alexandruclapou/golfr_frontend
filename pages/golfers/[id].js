2a41a3d999e2e30c94b8f875d0cc12a7c9853751
Alexandru Clapou <alexandru.clapou@golfgenius.com>
2022-02-24 10:45:36 +0200

    added golfer page

---
+++ pages/golfers/[id].js
@@ -0,0 +1,36 @@
+import Layout from '../../components/Layout'
+import { useRouter } from 'next/router'
+import useUserScores from '../../lib/useUserScores'
+import ScoreCard from '../../components/ScoreCard'
+
+export default function Golfer() {
+  const router = useRouter()
+  const userId = router.query.id
+  const { userScores, error } = useUserScores(userId)
+  const name = userScores ? userScores.user.name : ''
+  const scores = userScores ? userScores.user_scores : []
+  return (
+    <Layout>
+      {error ? (
+        <>
+          <p>User not found</p>
+        </>
+      ) : (
+        <>
+          <p className="tracking-widest">{name}</p>
+          {scores &&
+            scores.map(score => (
+              <ScoreCard
+                key={score.id}
+                id={score.id}
+                totalScore={score.total_score}
+                playedAt={score.played_at}
+                userId={userId}
+                userName={name}
+              />
+            ))}{' '}
+        </>
+      )}
+    </Layout>
+  )
+}
